a0caecd9fb12cf4cdcb2758366b6d51c14abeba9
kirargomedo23 <kirargomedo@gmail.com>
2023-01-21 17:25:28 -0500

    estructuración y correción

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch:'full'
+  },
+  {
+    path: 'app',
     children: [
       {
-        path: '',
-        loadChildren: () => import('@app/pages/login/login.module').then((m) => m.LoginModule),
+        path: 'products',
+        loadChildren: () => import('@app/pages/products/products.module').then((m) => m.ProductsModule),
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('@app/pages/profile/profile.module').then((m) => m.ProfileModule),
       }
     ]
+  },
+  {
+    path:'login',
+    loadChildren: () => import('@app/pages/login/login.module').then((m) => m.LoginModule),
   }
 ];
 
